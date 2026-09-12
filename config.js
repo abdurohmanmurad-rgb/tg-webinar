ec0466544f9989ea210a08d706899d81147cd7be
abdurahmanmurad <abdurohmanmurad@gmail.com>
2026-09-12 20:19:30 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.CFG = {
   /* Apps Script Web App manzili. Deploy qilgandan keyin /exec bilan tugaydigan
      to'liq havolani shu yerga qo'ying. Bo'sh qolsa sayt ishlaydi, lekin
      lid yuborilmaydi (konsolda ogohlantirish chiqadi). */
-  LID_URL: "",
+  LID_URL: "https://script.google.com/macros/s/AKfycbxHdmep7Qr3tLN1YfWOqq0CLX2K_2rvu9O1kcZ66RipMh7QOoJ1I1sjehZT8uTlB3AW6Q/exec",
 
   /* Analitikada shu nom bilan ajratiladi */
   SAYT: "tg-webinar"
